ce3f29b7f890306e81e977af5a8ece8fd54713c7
ilhan <ilhannn@gmail.com>
2015-11-13 09:09:15 +0200

    Changes to be committed: 	new file:   deneme.xlsx 	modified:   stv.py 	new file:   stv2.py

--- stv2.py
+++ stv2.py
@@ -11,6 +11,8 @@ numBireyOy = 2
 numSecmen = 20
 mQuota = 0
 lineStr=""
+electedCandidates = []
+eliminatedCandidates = []
 
 def createOylar():
 	#wbFilename = raw_input("Dosya adini gir: ")
@@ -202,12 +204,41 @@ def loadOylar(wb):
 		tumOylar[key]= int(countStr)
 	return tumOylar
 
+
+
+
+def hasOngoingCandidate(ballotPaper):
+	global electedCandidates
+	global eliminatedCandidates
+	for i in range(len(ballotPaper)):
+		if ballotPaper[i] not in electedCandidates and ballotPaper[i] not in eliminatedCandidates :
+			return True
+	return False
+
+def getContinuingCandidate(ballotPaper):
+	global electedCandidates
+	global eliminatedCandidates
+	for i in range(len(ballotPaper)):
+		if ballotPaper[i] not in electedCandidates and ballotPaper[i] not in eliminatedCandidates :
+			return ballotPaper[i]
+	return -1
+
+def newArrayWithZeros(size):
+	array = []
+	for i in size:
+		array.append(0)
+
+
 def main(argv):
 	global mQuota
 	global numSandalye
 	global numSecmen
 	global lineStr
-
+	global numAday
+	
+	ballotPaperValues = []
+	candidateVotes = newArrayWithZeros(numAday)
+	
 	if(len(argv)>1):
 		wb = load_workbook(filename = argv[1])
 		loadSecimDegiskenleri(wb)
@@ -218,7 +249,37 @@ def main(argv):
 		tumOylar = createOylar()
 		saveOylar(wb, tumOylar)
 
+	for i in range(len(tumOylar)):
+		if hasOngoingCandidate(tumOylar[i]) == False:
+			#(a) Any ballot paper that does not express a transferable preference for a continuing candidate is declared exhausted-without-value and recorded with a value of zero.
+			ballotPaperValues[i] = 0
+		else :
+			#(b) Assign each ballot paper allocated to a Continuing Candidate a Vote value of one.
+			candidate = getContinuingCandidate(tumOylar[i])
+			candidateVotes[candidate] += 1
+
+
+	#(c) Ascertain and assign the Candidate’s Total Value of the Vote (Ctvv) for each Continuing Candidate by aggregating the value of the votes allocated to each Continuing Candidate.
+	cctvs = adayOylar(tumOylar) # Candidate's total value of votes
+	#(d) Ascertain and assign the Total Vote (Tv) value by aggregating the total value of votes allocated to each candidate outlined in (c) above.
+	tv = sumOylar(cctvs)
+	#(e) Calculate the Quota required to elect a candidate by dividing the Total Vote (Tv) by 1 more than the number of candidates required to be elected and by increasing the quotient (disregarding any remainder) by 1 (Q = integer(Tv/(1+No of Vacancies))+1).
+	mQuota = int(math.floor((tv/ (numSandalye-len(electedCandidates)+1) + 1)))
+
+
+	#Provisional declaration of elected candidates
+	#Any candidate who has received a Total value of votes (Ctvv) equal to or greater than the Quota (Q) is to be provisionally declared elected.
+
+	for i in range(len(cctvs)):
+		if cctvs[i]>=mQuota:
+			electedCandidates.append(i)
+
+	#######https://en.wikipedia.org/wiki/Wright_system#Definitions
+
+
+
 	wsRounds = wb.create_sheet("Rounds")
+
 
 	mQuota = int(math.floor((numSecmen/ (numSandalye+1) + 1)))
 	print "Kota: " + str(mQuota)
